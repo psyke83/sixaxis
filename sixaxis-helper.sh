24cfa5c6076df09b86b13e0ee59df330dff7bcf3
Conn O'Griofa <connogriofa@gmail.com>
2018-01-19 16:24:39 +0000

    Remove ID_BUS variable parsing
    Breaks the script on Raspbian jessie due to udev version being too old.
    Running the timeout daemon shouldn't interfere with USB connections.

--- sixaxis-helper.sh
+++ sixaxis-helper.sh
@@ -17,7 +17,7 @@ fi
 
 # export udev variables into systemd service context
 eval $(udevadm info --query=env --export "/sys$UDEV_ENV")
-if [[ -z "$NAME" ]] || [[ -z "$UNIQ" ]] || [[ -z "$ID_BUS" ]]; then
+if [[ -z "$NAME" ]] || [[ -z "$UNIQ" ]]; then
     # we're not interested in the /jsX and /eventX udev events.
     exit 0
 fi
@@ -33,6 +33,7 @@ sixaxis_detect() {
     for event in /sys/class/input/event[0-9]*; do
         if [[ "$(cat $event/device/uniq)" == "$SIXAXIS_MAC" && "$(cat $event/device/name)" == "${NAME//\"/}" ]]; then
             SIXAXIS_DEVICE="${event//\/sys\/class/\/dev}"
+            SIXAXIS_NAME="${NAME//\"/} (${SIXAXIS_MAC^^})"
         fi
     done
 }
@@ -40,34 +41,31 @@ sixaxis_detect() {
 sixaxis_calibrate() {
     local axis
 
-    echo "Calibrating: $SIXAXIS_DEVICE"
+    echo "Calibrating: $SIXAXIS_NAME"
     for axis in ABS_X ABS_Y ABS_RX ABS_RY; do
         libevdev-tweak-device --abs "$axis" --fuzz 3 "$SIXAXIS_DEVICE"
     done
 }
 
 sixaxis_timeout() {
-    echo "$SIXAXIS_TIMEOUT second timeout set for: $SIXAXIS_DEVICE"
+    echo "Setting $SIXAXIS_TIMEOUT second timeout: $SIXAXIS_NAME"
     sixaxis-timeout "$SIXAXIS_DEVICE" "$SIXAXIS_TIMEOUT"
     if [[ "$?" -eq 1 ]]; then
-        echo "Disconnecting: $SIXAXIS_DEVICE"
+        echo "Disconnecting: $SIXAXIS_NAME"
         send_bluezcmd "disconnect ${SIXAXIS_MAC^^}"
     fi
 }
 
 sixaxis_settrust() {
-    echo "Setting Bluetooth trust: ${SIXAXIS_MAC^^}"
+    echo "Setting Bluetooth trust: $SIXAXIS_NAME"
     send_bluezcmd "trust ${SIXAXIS_MAC^^}"
 }
 
 sixaxis_detect
 if [[ -n "$SIXAXIS_DEVICE" ]]; then
     sixaxis_calibrate
-    if [[ "$ID_BUS" == "usb" ]]; then
-        sixaxis_settrust
-    elif [[ "$ID_BUS" == "bluetooth" ]]; then
-        sixaxis_timeout
-    fi
+    sixaxis_settrust
+    sixaxis_timeout
 fi
 
 exit 0
