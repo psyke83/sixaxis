1e06149b3509c5abe755588b073b35b2c5ff1ef6
Conn O'Griofa <connogriofa@gmail.com>
2018-02-07 11:28:02 +0000

    Simplify udev to systemd service interaction
    * Refine udev rules to only trigger one time per connection
    * Only pass event device node to systemd service
    * Modify script to query device name & mac address via sysfs node instead
      of relying on dodgy "unmangle" function.

--- sixaxis-helper.sh
+++ sixaxis-helper.sh
@@ -1,43 +1,13 @@
 #!/bin/bash
 
 ##
-## Configure native sony-hid driver for USB & Bluetooth(sixaxis)
+## Configure native sony-hid driver for USB & Bluetooth (sixaxis)
 ##
 
+SIXAXIS_DEVICE="$1"
+SIXAXIS_MAC="$(cat ${SIXAXIS_DEVICE/\/dev/\/sys\/class}/device/uniq)"
+SIXAXIS_NAME="$(cat ${SIXAXIS_DEVICE/\/dev/\/sys\/class}/device/name) (${SIXAXIS_MAC^^})"
 [[ -z "$SIXAXIS_TIMEOUT" ]] && SIXAXIS_TIMEOUT=600
-
-unmangle_path() {
-    local adjust
-    local path
-    local fullpath
-    for path in ${1//// }; do
-        if [[ "$adjust" -eq 1 ]]; then
-            fullpath+="-$path"
-            adjust=0
-        else
-            fullpath+="/$path"
-        fi
-
-        if [[ ! -e "$fullpath" ]]; then
-            adjust=1
-        fi
-     done
-
-     echo "$fullpath"
-}
-
-UDEV_DEVPATH="/sys$1"
-# unmangle path (systemd brain-damage): https://github.com/systemd/systemd/issues/5072
-if [[ ! -e "$UDEV_DEVPATH" ]]; then
-    UDEV_DEVPATH="$(unmangle_path /sys$1)"
-fi
-
-# export udev variables into systemd service context
-eval $(udevadm info --query=env --export "$UDEV_DEVPATH")
-if [[ -z "$NAME" ]] || [[ -z "$UNIQ" ]]; then
-    # we're not interested in the /jsX and /eventX udev events.
-    exit 0
-fi
 
 function send_bluezcmd() {
     # create a named pipe & fd for input for bluetoothctl
@@ -70,18 +40,6 @@ function send_bluezcmd() {
     exec 3>&-
 }
 
-sixaxis_detect() {
-    local event
-    SIXAXIS_MAC="${UNIQ//\"/}"
-
-    for event in /sys/class/input/event[0-9]*; do
-        if [[ "$(cat $event/device/uniq)" == "$SIXAXIS_MAC" && "$(cat $event/device/name)" == "${NAME//\"/}" ]]; then
-            SIXAXIS_DEVICE="${event//\/sys\/class/\/dev}"
-            SIXAXIS_NAME="${NAME//\"/} (${SIXAXIS_MAC^^})"
-        fi
-    done
-}
-
 sixaxis_calibrate() {
     local axis
 
@@ -92,7 +50,7 @@ sixaxis_calibrate() {
 }
 
 sixaxis_timeout() {
-    echo "Setting $SIXAXIS_TIMEOUT second timeout: $SIXAXIS_NAME"
+    echo "Setting $SIXAXIS_TIMEOUT second timeout on: $SIXAXIS_NAME"
     sixaxis-timeout "$SIXAXIS_DEVICE" "$SIXAXIS_TIMEOUT"
     if [[ "$?" -eq 1 ]]; then
         echo "Disconnecting: $SIXAXIS_NAME"
@@ -100,10 +58,7 @@ sixaxis_timeout() {
     fi
 }
 
-sixaxis_detect
-if [[ -n "$SIXAXIS_DEVICE" ]]; then
-    sixaxis_calibrate
-    sixaxis_timeout
-fi
+sixaxis_calibrate
+sixaxis_timeout
 
 exit 0
